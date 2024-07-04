d90eb82929ae3fa020f70a89e0d7657fe2d29a86
yaleshen0 <qhshen22@gmail.com>
2024-07-04 14:12:50 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
-
+    command=input()
+    sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
