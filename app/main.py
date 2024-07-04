e1e9b62e232a64a669a0e64e446bcf0e1fdfc59f
yaleshen0 <qhshen22@gmail.com>
2024-07-04 14:28:48 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    # sys.stdout.write("$ ")
+    # sys.stdout.flush()
 
     # Wait for user input
-    command=input()
-    sys.stdout.write(f"{command}: command not found\n")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command=input()
+        sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
