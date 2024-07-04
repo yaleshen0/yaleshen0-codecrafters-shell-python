cbdde837f2440214986706f769c54599a7001b81
yaleshen0 <qhshen22@gmail.com>
2024-07-04 14:32:43 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command=input()
+        if command == 'exit 0':
+            break
         sys.stdout.write(f"{command}: command not found\n")
+        
 
 if __name__ == "__main__":
     main()
